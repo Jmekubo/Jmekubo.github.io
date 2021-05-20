ab4604fe3e5449b32f92d5bae3fe59d60a1a1b7d
Jmekubo <johnmekubo@gmail.com>
2021-05-20 16:14:41 +0300

    added index.js and updated index.html files

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "jmekubo8@gmail.com",
+	Password : "Jayeem78&",
+	To : 'johnmekubo@yahoo.com',
+	From : "Email Testing",
+	Subject : "<email subject>",
+	Body : "Testing sssssss",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
