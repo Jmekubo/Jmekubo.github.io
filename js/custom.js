c0542359709bbb0b66498b12199ce5c5743ceffe
Jmekubo <johnmekubo@gmail.com>
2021-08-03 12:27:53 +0300

    added modified custom.js file

--- js/custom.js
+++ js/custom.js
@@ -105,21 +105,15 @@ $(document).ready(function () {
 
     //animatedModal
     $("#demo01").animatedModal();
-<<<<<<< HEAD
-<<<<<<< HEAD
     $("#demo02").animatedModal2({
     modalTarget: 'animatedModal2'
 	);
-=======
 	$("#demo02").animatedModal({
     modalTarget: 'animatedModal2'
 	});
->>>>>>> parent of 9e84852... Update custom.js
-=======
 	$("#demo02").animatedModal({
     modalTarget: 'animatedModal2'
 	});
->>>>>>> 7100edf4a6fc6827fce9351c1d15ff4174ba6cb5
 	$("#demo03").animatedModal({
     modalTarget: 'animatedModal3'
 	});
@@ -144,17 +138,14 @@ $(document).ready(function () {
 	$("#demo11").animatedModal({
     modalTarget: 'animatedModal20'
 	});
-<<<<<<< HEAD
 	$("#demo112").animatedModal({
     modalTarget: 'animatedModal12'
 	});
 	$("#demo113").animatedModal({
-=======
 	$("#demo12").animatedModal({
     modalTarget: 'animatedModal12'
 	});
 	$("#demo13").animatedModal({
->>>>>>> 7100edf4a6fc6827fce9351c1d15ff4174ba6cb5
     modalTarget: 'animatedModal13'
 	});
 	
@@ -162,18 +153,16 @@ $(document).ready(function () {
 	
 	// popup-modal
 	$("#animatedModal").popup-modal();
-<<<<<<< HEAD
-<<<<<<< HEAD
 	$("#animatedModal2").popup-modal2({
    	modalTarget: 'popup-modal12'
 	);
-	// $("#animatedModal12").popup-modal({
-   // modalTarget: 'popup-modal12'
-//	});
-	//$("#animatedModal13").popup-modal({
-   // modalTarget: 'popup-modal13'
-	// });
-=======
+	$("#animatedModal12").popup-modal({
+modalTarget: 'popup-modal12'
+	});
+	$("#animatedModal13").popup-modal({
+modalTarget: 'popup-modal13'
+	});
+
 	$("#animatedModal2").popup-modal({
     modalTarget: 'popup-modal2'
 	});
@@ -183,8 +172,6 @@ $(document).ready(function () {
 	$("#animatedModal13").popup-modal({
     modalTarget: 'popup-modal13'
 	});
->>>>>>> parent of 9e84852... Update custom.js
-=======
 	$("#animatedModal2").popup-modal({
     modalTarget: 'popup-modal'
 	});
@@ -194,7 +181,6 @@ $(document).ready(function () {
 	$("#animatedModal13").popup-modal({
     modalTarget: 'popup-modal'
 	});
->>>>>>> 7100edf4a6fc6827fce9351c1d15ff4174ba6cb5
 
     // Contact Form 	
 
